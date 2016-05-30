8fc3201927d4b3599202533609c922a5858f49cc
Dave Degeatano <davey.dee33@gmail.com>
2016-05-30 14:08:49 -0500

    Implemented POST method now too!
    Implemented POST function.  Before was just GET method.  I added a
    second input field, and a second javascript function for POST.  Nice to
    compare the 2 functions and see the differences of using each, very
    similar.  Also updated server.php, which could have been simpler, but I
    also added a switch statement in there really just for prepending “GET”
    or “POST” to the output so I could easily see which method was used.

--- server.php
+++ server.php
@@ -1,8 +1,23 @@
 <?php
 
-  $data = $_GET;
-  $user_text = $_GET['user_text'];
+  $method = $_SERVER['REQUEST_METHOD'];
+  $user_text = $_REQUEST['user_text']; // Use $_REQUEST if we don't know if it's $_POST or $_GET
   $response = strtoupper($user_text);
+
+// Can comment out this whole switch statement now, only keep to prepend the response to see if it came by POST or GET
+  switch ($method) {
+    case 'GET':
+      //$user_text = $_GET['user_text'];  // replaced by $_REQUEST above
+      $response = "GET: " . $response;
+      break;
+    case 'POST':
+      //$user_text = $_POST['user_text'];  // replaced by $_REQUEST above
+      $response = "POST: " . $response;
+      break;
+    default:
+      $response = "There was an error...";
+      break;
+  }
 
   echo $response;
 
